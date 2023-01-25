8926b81c8b3cd2649f3697908a0442075b782197
Guard Boi <105892653+mariohero24@users.noreply.github.com>
2023-01-24 21:54:08 -0800

    Update compress.py

--- compress.py
+++ compress.py
@@ -3,4 +3,4 @@ import json
 with open("docs.json") as f1:
  e = json.load(f1)
 with open("compressed.json", "w") as f2:
- json.dump(e, f2, seperators=(",", ":"))
+ json.dump(e, f2, separators=(',',':'))
